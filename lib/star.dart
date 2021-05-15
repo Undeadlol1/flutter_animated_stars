77395b4af9318fa9bf9feff807155ae0574fd48e
undeadlol1 <undeadlol1@mail.ru>
2021-05-15 16:52:20 +0500

    feat: :dizzy: add simple shooting star animation

---
+++ lib/star.dart
@@ -0,0 +1,56 @@
+import 'dart:math' as Math;
+import 'package:flutter/material.dart';
+import 'package:flutter_animated_stars/stars_painter.dart';
+
+class Star extends StatefulWidget {
+  const Star({Key key}) : super(key: key);
+
+  @override
+  _StarState createState() => _StarState();
+}
+
+class _StarState extends State<Star> with TickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController controller;
+  Tween<double> _rotationTween = Tween(begin: -Math.pi, end: Math.pi);
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 4),
+    );
+
+    animation = _rotationTween.animate(controller)
+      ..addListener(() => setState(() {}))
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller.repeat();
+        } else if (status == AnimationStatus.dismissed) {
+          controller.forward();
+        }
+      });
+
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: CustomPaint(
+        child: Container(),
+        painter: StarsPainter(
+          starPosition: animation.value,
+        ),
+      ),
+    );
+  }
+}
--- lib/stars_painter.dart
+++ lib/stars_painter.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 Paint _starPaint = Paint()..color = Colors.white;
 
 class StarsPainter extends CustomPainter {
+  double starPosition = 0.0;
+  StarsPainter({this.starPosition});
+
   @override
   void paint(Canvas canvas, Size size) {
     canvas.drawColor(Colors.black, BlendMode.clear);
@@ -12,7 +15,11 @@ class StarsPainter extends CustomPainter {
   }
 
   void _drawStarHead(Canvas canvas, Size size) {
-    canvas.drawCircle(Offset(size.width / 2, size.height / 2), 3, _starPaint);
+    canvas.drawCircle(
+      Offset((size.width / 2) * starPosition, size.height / 2),
+      3,
+      _starPaint,
+    );
   }
 
   void _drawStarTail(Canvas canvas, Size size) {
@@ -20,21 +27,30 @@ class StarsPainter extends CustomPainter {
     final horizontalCenter = size.width / 2;
     final verticallyCenter = size.height / 2;
 
-    for (var i = 0; i < 25; i++) {
-      canvas.drawCircle(
-        Offset(
-          horizontalCenter - Random().nextInt(100),
-          verticallyCenter +
-              (Random().nextBool() ? 1 : -1) * Random().nextInt(5),
-        ),
-        1,
-        color,
-      );
-    }
+    canvas.drawCircle(
+      Offset(
+        (size.width / 2) * starPosition - Random().nextInt(100),
+        verticallyCenter + (Random().nextBool() ? 1 : -1) * Random().nextInt(5),
+      ),
+      1,
+      color,
+    );
+
+    // for (var i = 0; i < 25; i++) {
+    //   canvas.drawCircle(
+    //     Offset(
+    //       horizontalCenter - Random().nextInt(100),
+    //       verticallyCenter +
+    //           (Random().nextBool() ? 1 : -1) * Random().nextInt(5),
+    //     ),
+    //     1,
+    //     color,
+    //   );
+    // }
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
-    return false;
+    return true;
   }
 }
