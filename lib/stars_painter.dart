90d7c2af07fc76e18fee6182032fe26701263247
undeadlol1 <undeadlol1@mail.ru>
2021-05-17 16:30:56 +0500

    refactor: :recycle: stars painter

--- lib/stars_painter.dart
+++ lib/stars_painter.dart
@@ -5,10 +5,15 @@ Paint _starPaint = Paint()..color = Colors.white;
 class StarsPainter extends CustomPainter {
   double starPosition = 0.0;
 
+  late Size _size;
+  late Canvas _canvas;
+
   StarsPainter({required this.starPosition});
 
   @override
   void paint(Canvas canvas, Size size) {
+    _size = size;
+    _canvas = canvas;
     canvas.clipRect(
       Rect.fromPoints(
         Offset(0, 0),
@@ -16,91 +21,78 @@ class StarsPainter extends CustomPainter {
       ),
     );
     canvas.drawColor(Colors.black, BlendMode.clear);
-    canvas.drawCircle(
-      Offset(size.width / 2, size.height / 2),
-      100,
-      Paint()..color = Colors.red,
-    );
-    _drawCanvasBorders(canvas, size);
-    _drawCrossSection(canvas, size);
-    // for (var i = 0; i < 10; i++) {
-    //   drawAStar(
-    //     size: size,
-    //     canvas: canvas,
-    //     horizontalPositon: (size.width / 4),
-    //     verticalPosition: (size.height / 8) + (i * 13),
-    //   );
-    // }
+    _drawCanvasBorders();
+    _drawCrossSection();
+
+    for (var i = 0; i < 1; i++) {
+      drawAStar(
+        horizontalPositon: (size.width / 2) + i,
+        verticalPosition: (size.height / 2) + i,
+      );
+    }
   }
 
-  void _drawCrossSection(Canvas canvas, Size size) {
-    debugPrint('size.width: ${size.width}');
-    debugPrint('size.height: ${size.height}');
-    var paint = Paint()
+  void _drawCrossSection() {
+    debugPrint('size.width: ${_size.width}');
+    debugPrint('size.height: ${_size.height}');
+    final paint = Paint()
       ..strokeWidth = 2
       ..color = Colors.deepPurple;
 
-    canvas.drawCircle(
-      Offset(size.width, 0),
-      20,
-      _starPaint,
-    );
-    canvas.drawLine(
+    _canvas.drawLine(
       Offset(0, 0),
-      Offset(size.width, size.height),
+      Offset(_size.width, _size.height),
       paint,
     );
-    canvas.drawLine(
-      Offset(size.width, 0),
-      Offset(0, size.height),
+    _canvas.drawLine(
+      Offset(_size.width, 0),
+      Offset(0, _size.height),
       paint,
     );
   }
 
-  void _drawCanvasBorders(Canvas canvas, Size size) {
+  void _drawCanvasBorders() {
     var paint = Paint()
       ..strokeWidth = 10
       ..color = Colors.deepPurple;
 
-    canvas.drawLine(
+    _canvas.drawLine(
       Offset(0, 0),
-      Offset(size.width, 0),
+      Offset(_size.width, 0),
       paint,
     );
-    canvas.drawLine(
-      Offset(size.width, 0),
-      Offset(size.width, size.height),
+    _canvas.drawLine(
+      Offset(_size.width, 0),
+      Offset(_size.width, _size.height),
       paint,
     );
-    canvas.drawLine(
+    _canvas.drawLine(
       Offset(0, 0),
-      Offset(0, size.height),
+      Offset(0, _size.height),
       paint,
     );
-    canvas.drawLine(
-      Offset(0, size.height),
-      Offset(size.width, size.height),
+    _canvas.drawLine(
+      Offset(0, _size.height),
+      Offset(_size.width, _size.height),
       paint,
     );
   }
 
   void drawAStar({
-    required Size size,
-    required Canvas canvas,
     required double horizontalPositon,
     required double verticalPosition,
   }) {
     _drawStarHead(
-      size: size,
-      canvas: canvas,
+      size: _size,
+      canvas: _canvas,
       position: Offset(horizontalPositon, verticalPosition),
     );
-    _drawStarTail(
-      size: size,
-      canvas: canvas,
-      verticalPosition: verticalPosition,
-      horizontalPositon: horizontalPositon,
-    );
+    // _drawStarTail(
+    //   size: size,
+    //   canvas: canvas,
+    //   verticalPosition: verticalPosition,
+    //   horizontalPositon: horizontalPositon,
+    // );
   }
 
   void _drawStarHead({
